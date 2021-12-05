0ec662bcd26a905c61f93912c8af19566d60e2e1
2jun0 <soo28819@naver.com>
2021-12-06 00:16:14 +0900

    add: add d.ts files

---
+++ dist/core.d.ts
@@ -0,0 +1,2 @@
+export declare function downloadFromWebsite(filePath: string, platform: string): Promise<unknown>;
+export declare function downloadFromGithub(filePath: string, version: string, platform: string): Promise<unknown>;
