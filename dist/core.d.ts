d13bc6c999c4a083965eb7490d51f528342397ec
2jun0 <soo28819@naver.com>
2021-12-06 02:21:05 +0900

    del: dist directory

--- dist/core.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function downloadFromWebsite(filePath: string, platform: string): Promise<unknown>;
-export declare function downloadFromGithub(filePath: string, version: string, platform: string): Promise<unknown>;
